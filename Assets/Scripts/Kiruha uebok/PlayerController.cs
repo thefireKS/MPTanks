dd0c789b6ce9572d8055e00ce156d56d2dc602f6
Nikita <84981163+MorgoWizard@users.noreply.github.com>
2023-03-25 19:57:52 +0300

    Collision check

--- Assets/Scripts/Kiruha uebok/PlayerController.cs
+++ Assets/Scripts/Kiruha uebok/PlayerController.cs
@@ -1,13 +1,16 @@
+using System;
 using Photon.Pun;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviourPunCallbacks
 {
     [SerializeField] private float speed = 5;
+    private Rigidbody2D _rigidbody2D;
     protected PhotonView view;
 
     private void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         view = GetComponent<PhotonView>();
     }
 
@@ -15,14 +18,22 @@ public class PlayerController : MonoBehaviourPunCallbacks
     {
         if (view.IsMine)
         {
+            
             Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
-            transform.position += (Vector3)moveAmount;
+            Vector2 moveAmount = moveInput.normalized * (speed * Time.deltaTime);
+            _rigidbody2D.velocity = moveAmount;
+            
+            //transform.position += (Vector3)moveAmount;
             
             if(moveInput.x == 0 && moveInput.y == 0) return;
             
             float rotationZ = Mathf.Atan2(moveInput.y,moveInput.x) * Mathf.Rad2Deg - 90f;
             transform.rotation = Quaternion.Euler(0f,0f,rotationZ);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        throw new NotImplementedException();
     }
 }
